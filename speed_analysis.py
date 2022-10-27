51c19ae4e4d3101de7861689dfe22a214cbd2507
EgeSarii <e.sari@science.ru.nl>
2022-10-27 18:59:48 +0200

    Uploaded the new progress about results for duckdb

--- speed_analysis.py
+++ speed_analysis.py
@@ -5,7 +5,7 @@ from REL.utils import process_results
 from REL.entity_disambiguation import EntityDisambiguation
 from REL.ner import Cmns, load_flair_ner
 
-def example_preprocessing(batch_size):
+def example_preprocessing_1(batch_size):
     # user does some stuff, which results in the format below.
     textList = []
     print("Started Reading JSON file which contains multiple JSON document")
@@ -17,12 +17,22 @@ def example_preprocessing(batch_size):
     processed = {}
     processed_list = []
     limit = 0
+
+    random_num_list = [3960,262,2654,1134,3468,584,1093,2458,4546,1714,
+                        4083,1839,469,2466,99,2586,1410,197,3758,76]
+
+    for num in random_num_list:
+        text = textList[num]['body']
+        new_item = {"test_doc{}".format(num):[text, []]}
+        processed_list.append(new_item)
+
+
     for i in range(1000):
         text = textList[i]['body']
         new_item = {"test_doc{}".format(i):[text, []]}
         processed_list.append(new_item)
         
-        '''if (limit<batch_size):
+    '''if (limit<batch_size):
             
             text = textList[i]['body']
             new_item = {"test_doc{}".format(i):[text, []]}
@@ -41,6 +51,28 @@ def example_preprocessing(batch_size):
     print(len(processed_list))
     return processed_list
 
+def example_preprocessing_2():
+   
+    textList = []
+    print("Started Reading JSON file which contains multiple JSON document")
+    with open('sample_5k') as f:
+        for jsonObj in f:
+            studentDict = json.loads(jsonObj)
+            textList.append(studentDict)
+    #print(len(textList))
+    processed_list = []
+
+    random_num_list = [3960,262,2654,1134,3468,584,1093,2458,4546,1714,
+                        4083,1839,469,2466,99,2586,1410,197,3758,76]
+
+    for num in random_num_list:
+        text = textList[num]['body']
+        new_item = {"test_doc{}".format(num):[text, []]}
+        processed_list.append(new_item)
+
+
+    print(len(processed_list))
+    return processed_list
 
 def entity_linking(input_text):
 
@@ -48,8 +80,6 @@ def entity_linking(input_text):
 
 
     base_url = "/home/ege/REL"
-    start_wall = time.time()
-    start_cpu = time.process_time()
 
     mention_detection = MentionDetection(base_url, wiki_version)
     tagger_ner = load_flair_ner("ner-fast")
@@ -67,26 +97,100 @@ def entity_linking(input_text):
     model = EntityDisambiguation(base_url, wiki_version, config)
     predictions, timing = model.predict(mentions_dataset)
 
-
     result = process_results(mentions_dataset, predictions, input_text)
 
-    end_wall = time.time()
-    end_cpu = time.process_time()
-    print("The wall-time is {}".format(end_wall-start_wall))
-    print("The cpu-time is {}".format(end_cpu-start_cpu))
-    return (((end_wall -start_wall), (end_cpu -start_cpu)))
+def speed_analysis_1():
+    wall_time_results = []
+    cpu_time_results = []
 
-wall_time_results = []
-cpu_time_results = []
+    for batch in example_preprocessing_1(1):
+        start_wall = time.time()
+        start_cpu = time.process_time()
 
-for batch in example_preprocessing(1):
-    result = entity_linking(batch)
-    wall_time_results.append(result[0])
-    cpu_time_results.append(result[1])
-    with open("wall_results_out_1000_sqlite.txt", "a") as o:
-        print(result[0], file=o)
-    with open("cpu_results_out_1000_sqlite.txt", "a") as o:
-        print(result[1], file=o)
+        result = entity_linking(batch)
+        
+        end_wall = time.time()
+        end_cpu = time.process_time()
+        
+        result_wall = end_wall-start_wall
+        result_cpu = end_cpu-start_cpu
+        print("The wall-time is {}".format(result_wall))
+        print("The cpu-time is {}".format(result_cpu))
+        
+        wall_time_results.append(result_wall)
+        cpu_time_results.append(result_cpu)
+        with open("wall_results_out_1000_sqlite.txt", "a") as o:
+            print(result_wall, file=o)
+        with open("cpu_results_out_1000_sqlite.txt", "a") as o:
+            print(result_cpu, file=o)
 
-print(wall_time_results)
-print(cpu_time_results)
+    print(wall_time_results)
+    print(cpu_time_results)
+
+def speed_analysis_2():
+    for batch in example_preprocessing_2():
+        
+        for i in range(10):
+            print(i)
+            entity_linking(batch)
+            with open("wall_query_CREATEINDEX_results_duckdb.txt", "a") as f :
+                f.write("\n")
+            with open("cpu_query_CREATEINDEX_results_duckdb.txt", "a") as f:
+                f.write("\n")
+
+            with open("wall_query_CLEAR_results_duckdb.txt", "a") as f :
+                f.write("\n")
+            with open("cpu_query_CLEAR_results_duckdb.txt", "a") as f:
+                f.write("\n")
+
+            with open("wall_query_INSERTBATCHEMB_results_duckdb.txt", "a") as f:
+                f.write("\n")
+            with open("cpu_query_INSERTBATCHEMB_results_duckdb.txt", "a") as f:
+                f.write("\n")
+
+            with open("wall_query_INSERTBATCHWIKI_results_duckdb.txt", "a") as f:
+                f.write("\n")
+            with open("cpu_query_INSERTBATCHWIKI_results_duckdb.txt", "a") as f:
+                f.write("\n")
+
+            with open("wall_query_LOOKUP_results_sqlite_1.txt", "a") as f:
+                f.write("\n")
+            with open("cpu_query_LOOKUP_results_sqlite_1.txt", "a") as f:
+                f.write("\n")
+
+            with open("wall_query_LOOKUPWIK_results_sqlite_1.txt", "a") as f:
+                f.write("\n")
+            with open("cpu_query_LOOKUPWIK_results_sqlite_1.txt", "a") as f:
+                f.write("\n")
+
+        with open("wall_query_CREATEINDEX_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+        with open("cpu_query_CREATEINDEX_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+
+        with open("wall_query_CLEAR_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+        with open("cpu_query_CLEAR_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+
+        with open("wall_query_INSERTBATCHEMB_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+        with open("cpu_query_INSERTBATCHEMB_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+
+        with open("wall_query_INSERTBATCHWIKI_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+        with open("cpu_query_INSERTBATCHWIKI_results_duckdb.txt", "a") as f:
+            f.write("\n\n")
+
+        with open("wall_query_LOOKUP_results_sqlite_1.txt", "a") as f:
+            f.write("\n\n")
+        with open("cpu_query_LOOKUP_results_sqlite_1.txt", "a") as f:
+            f.write("\n\n")
+
+        with open("wall_query_LOOKUPWIK_results_sqlite_1.txt", "a") as f:
+            f.write("\n\n")
+        with open("cpu_query_LOOKUPWIK_results_sqlite_1.txt", "a") as f:
+            f.write("\n\n")
+
+speed_analysis_2()
